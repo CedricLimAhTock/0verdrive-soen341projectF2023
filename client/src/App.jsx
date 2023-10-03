671c78daa7f7644ccc73cfd17b25b6bea218131f
Cedric Lim Ah Tock <c_limaht@live.concordia.ca>
2023-10-03 01:30:23 -0400

    Configure file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from "react";
 
 import "./App.css";
 // import Card from "./components/Card/Card";
-import Header from "./components/Header/Header";
-import SearchNPicture from "./components/SearchNPicture/SearchNPicture";
-import Container from "./components/Container/Container";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Container />
+      <h1>Counter</h1>
     </>
   );
 }
