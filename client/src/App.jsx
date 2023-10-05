8af9b36194143aa222e936342006e050c797af85
SoumyBhow <soumybhow1.0@gmail.com>
2023-10-05 19:44:44 -0400

    Get base of signin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,38 @@
-import react from 'react';
+import react from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-import Layout from './components/Layout';
-import Home from './pages/Home';
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <div>
+//           <Routes>
+//             <Route
+//               path="/"
+//               element={
+//                 <Layout>
+//                   <Home />
+//                 </Layout>
+//               }
+//             />
+//           </Routes>
+//         </div>
+//       </Router>
+//     </>
+//   );
+// }
 
 function App() {
-
-
-  return (
-    <>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<Layout><Home /></Layout>} />
-          </Routes>
-          </div> 
-      </Router>
-    </>
-  );
+  return <Signin />;
 }
 
 export default App;
