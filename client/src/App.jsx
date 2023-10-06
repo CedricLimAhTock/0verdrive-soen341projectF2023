b325232d63a87dbaa16ed7aff6849473d47a5548
Cedric Lim Ah Tock <c_limaht@live.concordia.ca>
2023-10-06 16:59:00 -0400

    Made singup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout><Home /></Layout>} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
