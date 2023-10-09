a55472f1326fef40856331ff5613612aa3c0e92d
SoumyBhow <soumybhow1.0@gmail.com>
2023-10-09 17:20:26 -0400

    Tweek signin page to match sign up routing thingamabob

--- client/src/components/SigninCard/SigninCard.jsx
+++ client/src/components/SigninCard/SigninCard.jsx
@@ -1,22 +1,58 @@
 import "./SigninCard.css";
 import "@fontsource/inter";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const SigninCard = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // try {
+    //     const res = await axios.post('http://localhost:5173/signup', {
+    //         username,
+    //         password
+    //     });
+
+    //     navigate("/login")
+    //     console.log(res.message);
+    // } catch (err) {
+    //     console.log(err);
+    // }
+  };
   return (
-    <div className = "signin-card-container"> 
-      <div className = "logo">Lorem Ipsum</div>
+    <div className="signin-card-container">
+      <div className="logo">Lorem Ipsum</div>
       <div className="signin-card">
         <h1>Sign In</h1>
         <div className="fields">
-          <div className="user">
-            <input type="text" placeholder="U S E R N A M E" className="signin-username"/>
-          </div>
-          <div className="pass">
-            <input type="text" placeholder="P A S S W O R D" className="signin-password"/>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="user">
+              <input
+                type="text"
+                placeholder="U S E R N A M E"
+                className="signin-username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="pass">
+              <input
+                type="text"
+                placeholder="P A S S W O R D"
+                className="signin-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </form>
         </div>
-        <div className="signinButton">
-          <button>Sign in</button>
-        </div>
+        <input type="submit" value="Sign In" className="signinButton" />
+        <Link to="/signup">Don't have an account yet?</Link>
       </div>
     </div>
   );
