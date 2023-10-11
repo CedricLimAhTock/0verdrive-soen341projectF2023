a9099fa1f13270ea6030173aec1a923b5b2bf120
SoumyBhow <soumybhow1.0@gmail.com>
2023-10-10 23:37:11 -0400

    Remove thumbnails cause we don't need them

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,15 +12,12 @@ const Home = () => {
     images: [
       {
         original: "https://picsum.photos/id/1018/1000/600/",
-        thumbnail: "https://picsum.photos/id/1018/250/150/",
       },
       {
         original: "https://picsum.photos/id/1015/1000/600/",
-        thumbnail: "https://picsum.photos/id/1015/250/150/",
       },
       {
         original: "https://picsum.photos/id/1019/1000/600/",
-        thumbnail: "https://picsum.photos/id/1019/250/150/",
       },
     ],
     price: "$1,000,000",
