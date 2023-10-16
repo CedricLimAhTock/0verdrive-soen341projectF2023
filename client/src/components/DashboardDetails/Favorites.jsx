b174de2165cd45fd6dd30bea570f47dd9bbac0ac
Cedric Lim Ah Tock <c_limaht@live.concordia.ca>
2023-10-15 20:48:00 -0400

    Fixed css

--- client/src/components/DashboardDetails/Favorites.jsx
+++ client/src/components/DashboardDetails/Favorites.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import PropertyCard from '../PropertyCard/PropertyCard'
+import './styles/Favorites.css'
 
 const Favorites = () => {
+
+  const property = {
+    images: [
+      {
+        original: "https://picsum.photos/id/1018/1000/600/",
+      },
+      {
+        original: "https://picsum.photos/id/1015/1000/600/",
+      },
+      {
+        original: "https://picsum.photos/id/1019/1000/600/",
+      },
+    ],
+    price: "$1,000,000",
+    address: "1234 Main St, San Diego, CA 92101",
+    bedrooms: 3,
+    bathrooms: 2,
+    size: 2000,
+  };
+
   return (
-    <div>Favorites</div>
+    <div className='favorites'>
+      <PropertyCard className='test' property={property}/>
+      <PropertyCard className='test' property={property}/>
+      <PropertyCard className='test' property={property}/>
+      <PropertyCard className='test' property={property}/>
+      <PropertyCard className='test' property={property}/>
+      <PropertyCard className='test' property={property}/>
+      <PropertyCard className='test' property={property}/>
+    </div>
   )
 }
 
