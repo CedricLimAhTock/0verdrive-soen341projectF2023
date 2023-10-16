0a61dee3c3abc36299d4e84ff090c8f0ea879199
SoumyBhow <soumybhow1.0@gmail.com>
2023-10-15 20:49:49 -0400

    Make expanded content slightly better

--- client/src/components/DashboardDetails/Bookings.jsx
+++ client/src/components/DashboardDetails/Bookings.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import BuyerBookingCard from './BuyerBookingCard';
-import './styles/Bookings.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import BuyerBookingCard from "./BuyerBookingCard";
+import "./styles/Bookings.css";
 
 const Bookings = ({ data }) => {
   const [expandedCard, setExpandedCard] = useState(null);
 
   const toggleExpand = (index) => {
-    console.log('expand1');
+    console.log("expand1");
     if (index === expandedCard) {
       setExpandedCard(null);
     } else {
@@ -23,11 +23,18 @@ const Bookings = ({ data }) => {
       price: "$1,000,000",
       broker: "John Doe",
     },
+    {
+      type: "Home",
+      status: "declined",
+      address: "308 Negra Arroyo Lane, Albuquerque, New Mexico 87104",
+      price: "$67,000,000",
+      broker: "Ben Dover",
+    },
     // Add more booking data as needed
   ];
 
   return (
-    <div className='bookings'>
+    <div className="bookings">
       <div className="booking-header">
         <div className="header-type">Type</div>
         <div className="header-status">Status</div>
