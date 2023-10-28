9bd717f1bfff3d84559151d146731a650a507246
SoumyBhow <soumybhow1.0@gmail.com>
2023-10-28 11:17:47 -0400

    Fix message box

--- client/src/components/VisitForm/VisitForm.jsx
+++ client/src/components/VisitForm/VisitForm.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import '../DashboardDetails/styles/Users.css';
-import jwt_decode from 'jwt-decode';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../DashboardDetails/styles/Users.css";
+import "./VisitForm.css";
+import jwt_decode from "jwt-decode";
 
-const VisitForm = ({ isFormOpen, closeForm, property}) => {
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [message, setMessage] = useState('');
+const VisitForm = ({ isFormOpen, closeForm, property }) => {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
 
   const [decodedToken, setDecodedToken] = React.useState(null);
 
   useEffect(() => {
-      function fetchData() {
+    function fetchData() {
       const token = localStorage.getItem("jwtToken");
       const decoded = jwt_decode(token);
       setDecodedToken(decoded);
@@ -25,28 +26,28 @@ const VisitForm = ({ isFormOpen, closeForm, property}) => {
 
     try {
       alert(`Email: ${email}\nPhone: ${phone}\nMessage: ${message}`);
-      const response = await axios.post('http:127.0.0.1:8080/visit/', {
+      const response = await axios.post("http:127.0.0.1:8080/visit/", {
         propertyId: property.id.toString(),
         client_id: decodedToken.id.toString(),
         broker_id: property.broker_id.toString(),
       });
 
       if (response.status === 200) {
-        alert('Form submitted');
+        alert("Form submitted");
         console.log(response);
       } else {
         console.log(response);
-        console.log('Failed to submit form');
+        console.log("Failed to submit form");
       }
     } catch (err) {
       alert(err.message);
-      console.log('Error submitting form');
+      console.log("Error submitting form");
       console.error(err);
     }
   };
 
   return (
-    <div className={isFormOpen ? 'show' : 'hide'}>
+    <div className={isFormOpen ? "show" : "hide"}>
       <form className="popup-form" onSubmit={handleSubmit}>
         <button onClick={closeForm}>Close</button>
         <h2>Visit Form</h2>
