aef70554c544b6f10589775145b52c332d6053f1
SoumyBhow <soumybhow1.0@gmail.com>
2023-10-30 14:18:00 -0400

    Fix signup card exported as signincard

--- client/src/components/SignupCard/SignupCard.jsx
+++ client/src/components/SignupCard/SignupCard.jsx
@@ -4,42 +4,43 @@ import axios from "axios";
 import "../SigninCard/SigninCard.css";
 import "@fontsource/inter";
 
-const SigninCard = () => {
+const SignupCard = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [userRole, setUserRole] = useState("Home Buyer");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-        try {
-            const res = await axios.post('http://127.0.0.1:8080/signup', {
-                username,
-                password
-            });
 
-            navigate("/signin")
-            console.log(res.message);
-        } catch (err) {
-            console.log(err);
-        }
-  }
+    try {
+      const res = await axios.post("http://127.0.0.1:8080/signup", {
+        username,
+        password,
+      });
+
+      navigate("/signin");
+      console.log(res.message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="signin-card-container">
       <div className="logo">Lorem Ipsum</div>
       <div className="login-container">
         <h2>Sign up</h2>
-        <div className = "form-container">
-        <form onSubmit={handleSubmit}>
-          <div className="form-element">
-            <input
-              type="text"
-              placeholder="U S E R N A M E"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <div className="form-element">
+              <input
+                type="text"
+                placeholder="U S E R N A M E"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
             </div>
             <div className="form-element">
               <input
@@ -54,11 +55,11 @@ const SigninCard = () => {
               <input type="submit" value="Sign up" />
               <Link to="/signin">Have an account? Log in here</Link>
             </div>
-        </form>
+          </form>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
 
-export default SigninCard;
+export default SignupCard;
