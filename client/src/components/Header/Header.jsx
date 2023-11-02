fa9103cd36ca2c3f89524234aa274c877a83389c
SoumyBhow <soumybhow1.0@gmail.com>
2023-11-02 12:02:14 -0400

    Noah done goofed

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,21 +4,21 @@ import "./Header.css";
 import darkToggle from "./assets/darkToggle.svg";
 import bulb from "./assets/bulb.svg";
 import hamburgerMenu from "./assets/hamburgerMenu.svg";
-import menuClose from "./assets/menu-close.svg";
-import {HiMiniBars3} from "react-icons/hi2"
-import {LiaTimesSolid} from "react-icons/lia"
+// import menuClose from "./assets/menu-close.svg";
+import { HiMiniBars3 } from "react-icons/hi2";
+import { LiaTimesSolid } from "react-icons/lia";
 
 export default function Header({ decodedToken }) {
   const displayUsername = decodedToken ? decodedToken.username : "Guest!";
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
   const handleClick = () => {
-    setClick(!click)
-  }
-  const closeMenu = () => setClick(false)
+    setClick(!click);
+  };
+  const closeMenu = () => setClick(false);
 
   return (
     <div className="header">
-      <NavLink to="/" className="name" > 
+      <NavLink to="/" className="name">
         Lorem Ipsum
       </NavLink>
       <div className="nav">
@@ -43,10 +43,10 @@ export default function Header({ decodedToken }) {
         <img className="bulb" src={bulb} alt="Bulb" />
         <img className="hamburgerMenu" src={hamburgerMenu} alt="Hamburger Menu" /> */}
         <div className="username-display">Hello, {displayUsername}</div>
-       
+
         <div className="menu-icon" onClick={handleClick}>
-        {click ? <LiaTimesSolid /> : <HiMiniBars3 />}
-          <div className={click ? "menu-items active" : "menu-items"} >
+          {click ? <LiaTimesSolid /> : <HiMiniBars3 />}
+          <div className={click ? "menu-items active" : "menu-items"}>
             {decodedToken ? (
               <NavLink to="/signout" className="menu-item">
                 Sign Out
