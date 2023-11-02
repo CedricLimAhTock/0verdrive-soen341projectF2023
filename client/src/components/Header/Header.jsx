db890427bb646884e11c8643f2e1b5a0a8236a65
Noah Burns <n14burns@gmail.com>
2023-11-02 02:52:31 -0400

    added hamburger animation

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,13 +4,21 @@ import "./Header.css";
 import darkToggle from "./assets/darkToggle.svg";
 import bulb from "./assets/bulb.svg";
 import hamburgerMenu from "./assets/hamburgerMenu.svg";
+import menuClose from "./assets/menu-close.svg";
+import {HiMiniBars3} from "react-icons/hi2"
+import {LiaTimesSolid} from "react-icons/lia"
 
 export default function Header({ decodedToken }) {
   const displayUsername = decodedToken ? decodedToken.username : "Guest!";
+  const [click, setClick] = useState(false)
+  const handleClick = () => {
+    setClick(!click)
+  }
+  const closeMenu = () => setClick(false)
 
   return (
     <div className="header">
-      <NavLink to="/" className="name">
+      <NavLink to="/" className="name" > 
         Lorem Ipsum
       </NavLink>
       <div className="nav">
@@ -35,20 +43,17 @@ export default function Header({ decodedToken }) {
         <img className="bulb" src={bulb} alt="Bulb" />
         <img className="hamburgerMenu" src={hamburgerMenu} alt="Hamburger Menu" /> */}
         <div className="username-display">Hello, {displayUsername}</div>
-        <div className="headerMenu">
-          <img
-            className="hamburgerMenu"
-            src={hamburgerMenu}
-            alt="Hamburger Menu"
-          />
-          <div className="menuItems">
+       
+        <div className="menu-icon" onClick={handleClick}>
+        {click ? <LiaTimesSolid /> : <HiMiniBars3 />}
+          <div className={click ? "menu-items active" : "menu-items"} >
             {decodedToken ? (
-              <NavLink to="/signout" className="menuItem">
-                SIGN OUT
+              <NavLink to="/signout" className="menu-item">
+                Sign Out
               </NavLink>
             ) : (
-              <NavLink to="/signin" className="menuItem">
-                SIGN IN
+              <NavLink to="/signin" className="menu-item">
+                Sign In
               </NavLink>
             )}
           </div>
