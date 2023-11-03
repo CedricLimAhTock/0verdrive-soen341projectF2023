a9362cde044cf709484b06e67394718a42485a5a
Cedric Lim Ah Tock <c_limaht@live.concordia.ca>
2023-11-02 23:17:41 -0400

    Visit form

--- client/src/components/VisitForm/VisitForm.jsx
+++ client/src/components/VisitForm/VisitForm.jsx
@@ -3,12 +3,14 @@ import axios from "axios";
 import "../DashboardDetails/styles/Users.css";
 import "./VisitForm.css";
 import jwt_decode from "jwt-decode";
+import Carousel from "../Carousel/Carousel";
 
 const VisitForm = ({ isFormOpen, closeForm, property }) => {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
 
+  const { images, price, address, broker } = property;
   const [decodedToken, setDecodedToken] = React.useState(null);
 
   useEffect(() => {
@@ -51,40 +53,49 @@ const VisitForm = ({ isFormOpen, closeForm, property }) => {
       <form className="popup-form" onSubmit={handleSubmit}>
         <button onClick={closeForm} className='close-button'>Close</button>
         <h2>Visit Form</h2>
+        <div className="left-visit">
+          <Carousel images={images} className="carousel-visit" />
+          <div className="property-info-visit">
+            <h3>Price: ${price}</h3>
+            <h4>Address: {address}</h4>
+            <p>Broker: {broker}</p>
+          </div>
+        </div>
+        <div className="right-visit">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            id="phone"
+            type="tel"
+            value={phone}
+            placeholder="Phone number"
+            onChange={(e) => setPhone(e.target.value)}
+            required
+          />
 
-        <label htmlFor="phone">Phone Number</label>
-        <input
-          id="phone"
-          type="tel"
-          value={phone}
-          placeholder="Phone number"
-          onChange={(e) => setPhone(e.target.value)}
-          required
-        />
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            value={message}
+            placeholder="Enter your message here"
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
 
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          value={message}
-          placeholder="Enter your message here"
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        />
-
-        <div className="button-container">
-          <button type="submit" className="submit">
-            Submit
-          </button>
+          <div className="button-container">
+            <button type="submit" className="submit">
+              Submit
+            </button>
+          </div>
         </div>
       </form>
     </div>
