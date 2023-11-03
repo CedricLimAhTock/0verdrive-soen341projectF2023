df8522ebb87c94ab0ca0e452f907cd27843cc2a4
Kristi Doce <kristidoce@gmail.com>
2023-11-03 17:55:38 -0400

    fix conflict in sugnup

--- server/routes/auth/signup.js
+++ server/routes/auth/signup.js
@@ -8,83 +8,31 @@ import Broker from '../../models/broker.js';
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    try {
-        let data = req.body;
+  try {
+    let data = req.body;
       
-        const hashedPassword = await bcrypt.hash(data.password, 10);
-        data.password = hashedPassword;
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    data.password = hashedPassword;
 
-        const role = await Role.findOne({where: {type: data.userRole.toLowerCase().split(" ").join("")}});
+    const role = await Role.findOne({ where: { type: data.userRole.toLowerCase().split(" ").join("") } });
         
-        if(!role){
-            res.status(400).json({message: "Role does not exist."});
-        }
-
-        delete data.userRole;
-
-        const [user, created] = await User.findOrCreate({
-            where: data,
-            defaults: {
-                active: 1,
-                firstname: "",
-                lastname: "",
-                email: "",
-                phone: ""
-            }
-        });
-        if (!created) {
-            res.status(400).json({message: "Username already taken."});
-        } else {
-            // each new user must have a role associated
-            const user_role = await User_role.create({
-                active: 1,
-                user_id: user.id,
-                role_id: role.id
-            });
-
-            if(!user_role){
-                user.destroy();
-                res.status(400).json({message: "Failed to assign role."});
-            }
-
-            // if user is a broker, create an entry in broker table
-            if (role.type == 'broker') {
-                const broker = await Broker.create({
-                    active: 1,
-                    user_id: user.id,
-                    license_number: "",
-                    agency: "",
-                    email: "",
-                    phone: ""
-                });
-
-                if (!broker) {
-                    user.destroy();
-                    res.status(400).json({message: "Failed to add broker entry."});
-                }
-            }
-
-            res.status(200).send(user);
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Server error'
-        });
+    if (!role) {
+      res.status(400).json({ message: "Role does not exist." });
     }
 
     delete data.userRole;
 
-    [user, created] = await User.findOrCreate({
+    const [user, created] = await User.findOrCreate({
       where: data,
       defaults: {
         active: 1,
         firstname: "",
         lastname: "",
         email: "",
-        phone: "",
-      },
+        phone: ""
+      }
     });
+    
     if (!created) {
       res.status(400).json({ message: "Username already taken." });
     } else {
@@ -92,7 +40,7 @@ router.post('/', async (req, res) => {
       const user_role = await User_role.create({
         active: 1,
         user_id: user.id,
-        role_id: role.id,
+        role_id: role.id
       });
 
       if (!user_role) {
@@ -100,17 +48,30 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: "Failed to assign role." });
       }
 
+      // if user is a broker, create an entry in broker table
+      if (role.type == 'broker') {
+        const broker = await Broker.create({
+          active: 1,
+          user_id: user.id,
+          license_number: "",
+          agency: "",
+          email: "",
+          phone: ""
+        });
+
+        if (!broker) {
+          user.destroy();
+          res.status(400).json({ message: "Failed to add broker entry." });
+        }
+      }
+
       res.status(200).send(user);
     }
   } catch (err) {
     console.log(err);
-    if (!created) {
-      res.status(400).json({ message: "Username already taken." });
-    } else {
-      res.status(500).json({
-        message: "Server error",
-      });
-    }
+    res.status(500).json({
+      message: 'Server error'
+    });
   }
 });
 
