0af1013ac4ed1d776b4b22ae5903dde87959360f
SoumyBhow <soumybhow1.0@gmail.com>
2023-11-04 17:16:52 -0400

    Search (kinda) works

--- server/controllers/propertySearch.js
+++ server/controllers/propertySearch.js
@@ -6,6 +6,8 @@ const query = async (req, res) => {
     let fields = req.body.fields;
     let sort = req.body.sort;
 
+    console.log(fields);
+
     if (
       ![
         "price",
@@ -20,7 +22,7 @@ const query = async (req, res) => {
 
     sort.order = sort.order == "asc" ? "asc" : "desc";
 
-    let q = {};
+    let q = [];
     // if (fields.civicAddress) {
     //   q.civicAddress = { [Op.substring]: fields.civicAddress };
     // }
@@ -45,85 +47,134 @@ const query = async (req, res) => {
     if (fields.manyTerms) {
       let terms = fields.manyTerms.split(" ");
 
-      q.civicAddress = {
-        [Op.or]: terms.map((term) => ({ [Op.substring]: term })),
-      };
-      q.street = { [Op.or]: terms.map((term) => ({ [Op.substring]: term })) };
-      q.neighbourhood = {
-        [Op.or]: terms.map((term) => ({ [Op.substring]: term })),
-      };
-      q.city = { [Op.or]: terms.map((term) => ({ [Op.substring]: term })) };
-      q.province = { [Op.or]: terms.map((term) => ({ [Op.substring]: term })) };
+      q.push({
+        civicAddress: {
+          [Op.or]: terms.map((term) => ({ [Op.substring]: term })),
+        },
+      });
+      q.push({
+        street: { [Op.or]: terms.map((term) => ({ [Op.substring]: term })) },
+      });
 
-      q.country = { [Op.or]: terms.map((term) => ({ [Op.substring]: term })) };
+      q.push({
+        neighbourhood: {
+          [Op.or]: terms.map((term) => ({ [Op.substring]: term })),
+        },
+      });
+      q.push({
+        city: { [Op.or]: terms.map((term) => ({ [Op.substring]: term })) },
+      });
+
+      q.push({
+        province: {
+          [Op.or]: terms.map((term) => ({ [Op.substring]: term })),
+        },
+      });
+      q.push({
+        country: { [Op.or]: terms.map((term) => ({ [Op.substring]: term })) },
+      });
+      q.push({
+        propertyType: {
+          [Op.or]: terms.map((term) => ({ [Op.substring]: term })),
+        },
+      });
     }
 
-    if (fields.price) {
-      q.price = {
-        [Op.gte]: !fields.price.min ? 0 : fields.price.min,
-        [Op.lte]: !fields.price.max
-          ? Number.MAX_SAFE_INTEGER
-          : fields.price.max,
-      };
+    if (fields.price && (fields.price.min || fields.price.max)) {
+      q.push({
+        price: {
+          [Op.gte]: !fields.price.min ? 0 : fields.price.min,
+          [Op.lte]: !fields.price.max
+            ? Number.MAX_SAFE_INTEGER
+            : fields.price.max,
+        },
+      });
     }
-    if (fields.livingArea) {
-      q.livingArea = {
-        [Op.gte]: !fields.livingArea.min ? 0 : fields.livingArea.min,
-        [Op.lte]: !fields.livingArea.max
-          ? Number.MAX_SAFE_INTEGER
-          : fields.livingArea.max,
-      };
+    if (fields.livingArea && (fields.livingArea.min || fields.livingArea.max)) {
+      q.push({
+        livingArea: {
+          [Op.gte]: !fields.livingArea.min ? 0 : fields.livingArea.min,
+          [Op.lte]: !fields.livingArea.max
+            ? Number.MAX_SAFE_INTEGER
+            : fields.livingArea.max,
+        },
+      });
     }
-    if (fields.propertyArea) {
-      q.propertyArea = {
-        [Op.gte]: !fields.propertyArea.min ? 0 : fields.propertyArea.min,
-        [Op.lte]: !fields.propertyArea.max
-          ? Number.MAX_SAFE_INTEGER
-          : fields.propertyArea.max,
-      };
+    if (
+      fields.propertyArea &&
+      (fields.propertyArea.min || fields.propertyArea.max)
+    ) {
+      q.push({
+        propertyArea: {
+          [Op.gte]: !fields.propertyArea.min ? 0 : fields.propertyArea.min,
+          [Op.lte]: !fields.propertyArea.max
+            ? Number.MAX_SAFE_INTEGER
+            : fields.propertyArea.max,
+        },
+      });
     }
-    if (fields.numOfBedrooms) {
-      q.numOfBedrooms = {
-        [Op.gte]: !fields.numOfBedrooms.min ? 0 : fields.numOfBedrooms.min,
-        [Op.lte]: !fields.numOfBedrooms.max
-          ? Number.MAX_SAFE_INTEGER
-          : fields.numOfBedrooms.max,
-      };
+    if (
+      fields.numOfBedrooms &&
+      (fields.numOfBedrooms.min || fields.numOfBedrooms.max)
+    ) {
+      q.push({
+        numOfBedrooms: {
+          [Op.gte]: !fields.numOfBedrooms.min ? 0 : fields.numOfBedrooms.min,
+          [Op.lte]: !fields.numOfBedrooms.max
+            ? Number.MAX_SAFE_INTEGER
+            : fields.numOfBedrooms.max,
+        },
+      });
     }
-    if (fields.numOfBathrooms) {
-      q.numOfBathrooms = {
-        [Op.gte]: !fields.numOfBathrooms.min ? 0 : fields.numOfBathrooms.min,
-        [Op.lte]: !fields.numOfBathrooms.max
-          ? Number.MAX_SAFE_INTEGER
-          : fields.numOfBathrooms.max,
-      };
+    if (
+      fields.numOfBathrooms &&
+      (fields.numOfBathrooms.min || fields.numOfBathrooms.max)
+    ) {
+      q.push({
+        numOfBathrooms: {
+          [Op.gte]: !fields.numOfBathrooms.min ? 0 : fields.numOfBathrooms.min,
+          [Op.lte]: !fields.numOfBathrooms.max
+            ? Number.MAX_SAFE_INTEGER
+            : fields.numOfBathrooms.max,
+        },
+      });
     }
-    if (fields.numOfFloors) {
-      q.numOfFloors = {
-        [Op.gte]: !fields.numOfFloors.min ? 0 : fields.numOfFloors.min,
-        [Op.lte]: !fields.numOfFloors.max
-          ? Number.MAX_SAFE_INTEGER
-          : fields.numOfFloors.max,
-      };
+    if (
+      fields.numOfFloors &&
+      (fields.numOfFloors.min || fields.numOfFloors.max)
+    ) {
+      q.push({
+        numOfFloors: {
+          [Op.gte]: !fields.numOfFloors.min ? 0 : fields.numOfFloors.min,
+          [Op.lte]: !fields.numOfFloors.max
+            ? Number.MAX_SAFE_INTEGER
+            : fields.numOfFloors.max,
+        },
+      });
     }
-    if (fields.yearBuilt) {
-      q.yearBuilt = {
-        [Op.gte]: !fields.yearBuilt.min ? "1000-01-01" : fields.yearBuilt.min,
-        [Op.lte]: !fields.yearBuilt.max ? "9999-12-31" : fields.yearBuilt.max,
-      };
+    if (fields.yearBuilt && (fields.yearBuilt.min || fields.yearBuilt.max)) {
+      q.push({
+        yearBuilt: {
+          [Op.gte]: !fields.yearBuilt.min ? "1000-01-01" : fields.yearBuilt.min,
+          [Op.lte]: !fields.yearBuilt.max ? "9999-12-31" : fields.yearBuilt.max,
+        },
+      });
     }
-    if (fields.listedDate) {
-      q.listedDate = {
-        [Op.gte]: !fields.listedDate.min ? "1000-01-01" : fields.listedDate.min,
-        [Op.lte]: !fields.listedDate.max ? "9999-12-31" : fields.listedDate.max,
-      };
-    }
-    if (fields.propertyType) {
-      q.propertyType = fields.propertyType;
+    if (fields.listedDate && (fields.listedDate.min || fields.listedDate.max)) {
+      q.push({
+        listedDate: {
+          [Op.gte]: !fields.listedDate.min
+            ? "1000-01-01"
+            : fields.listedDate.min,
+          [Op.lte]: !fields.listedDate.max
+            ? "9999-12-31"
+            : fields.listedDate.max,
+        },
+      });
     }
 
     if (fields.postalCode) {
-      q.postalCode = fields.postalCode;
+      q.push({ postalCode: fields.postalCode }); //fields.postalCode;
     }
 
     console.log(q);
@@ -151,7 +202,7 @@ const query = async (req, res) => {
         "yearBuilt",
         "listedDate",
       ],
-      where: q,
+      where: { [Op.or]: q },
       order: [[sort.parameter, sort.order]],
     });
 
