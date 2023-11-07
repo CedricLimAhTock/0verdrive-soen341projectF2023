1289961d41561ab931d973dd28b8bb8df8216055
SoumyBhow <soumybhow1.0@gmail.com>
2023-11-07 12:05:33 -0500

    Clicking on broker redirects to broken page (progress!!!)

--- client/src/components/BrokerDetailedCard/BrokerDetailedCard.jsx
+++ client/src/components/BrokerDetailedCard/BrokerDetailedCard.jsx
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from "react";
-import template from "../../assets/slideshow-template.jpg";
+
 import "./BrokerDetailedCard.css";
-import bedIcon from "../../assets/bed.svg";
-import bathIcon from "../../assets/bath.svg";
-import rulerIcon from "../../assets/ruler.svg";
-import MortgageCalculator from "../MortgageCalculator/MortgageCalculator";
-import Carousel from "../Carousel/Carousel";
-import VisitForm from "../VisitForm/VisitForm";
-import OfferForm from "../OfferForm/OfferForm";
+
 import jwt_decode from "jwt-decode";
 
 const BrokerDetailedCard = ({ broker }) => {
-  const { firstname, lastname, email, phone, agency, id } = property;
+  const { firstname, lastname, email, phone, agency, id } = broker;
 
   const [decodedToken, setDecodedToken] = React.useState(null);
 
@@ -31,28 +25,12 @@ const BrokerDetailedCard = ({ broker }) => {
 
   const [activeTab, setActiveTab] = useState("description");
 
-  const [isFormOpen, setIsFormOpen] = useState(false);
-  const [isVisitForm, setVisitForm] = useState(false);
-  const [isOfferForm, setOfferForm] = useState(false);
-
-  const toggleVisitForm = () => {
-    setVisitForm(!isVisitForm);
-  };
-
-  const toggleForm = () => {
-    setIsFormOpen(!isFormOpen);
-  };
-
-  const toggleOfferForm = () => {
-    setOfferForm(!isOfferForm);
-  };
-
   return (
     <div className="details">
       <div className="left-side">
         <div className="info">
-          <h2 className="title">{price}</h2>
-          <p className="address">{address}</p>
+          <h2 className="title">{agency}</h2>
+          <p className="address">{email}</p>
         </div>
 
         <div className="tabs">
@@ -77,68 +55,14 @@ const BrokerDetailedCard = ({ broker }) => {
             </button>
           </div>
           <div className="property-details">
-            {activeTab === "description" && (
-              <>
-                <p>{description}</p>
-                <br />
-                <p>Neighbourhood: {neighbourhood}</p>
-              </>
-            )}
+            {activeTab === "description" && <p>{description}</p>}
             {activeTab === "broker" && <p>{broker}</p>}
-            {activeTab === "map" && <p>{address}</p>}
-          </div>
-        </div>
-
-        <b>Features</b>
-        <div className="property-features">
-          <div className="features">
-            <img className="c-icons" src={bedIcon} alt="Bed Icon" />
-            <span className="icon-numbers">{numOfBedrooms}</span>
-          </div>
-          <div className="features">
-            <img className="c-icons" src={bathIcon} alt="Bath Icon" />
-            <span className="icon-numbers">{numOfBathrooms}</span>
-          </div>
-          <div className="features">
-            <img className="c-icons" src={rulerIcon} alt="Ruler Icon" />
-            <span className="icon-numbers">{propertyArea} sq ft</span>
+            {activeTab === "map" && <p>{description}</p>}
           </div>
         </div>
       </div>
 
-      <div className="right-side">
-        <h2 className="price">{price}</h2>
-        <button className="offer" onClick={() => toggleOfferForm(true)}>
-          Make an offer
-        </button>
-        <button className="visit" onClick={() => toggleVisitForm(true)}>
-          Request a visit
-        </button>
-        <button className="calc" onClick={setIsFormOpen}>
-          Mortgage Calculator
-        </button>
-        <MortgageCalculator
-          isOpen={isFormOpen}
-          onClose={toggleForm}
-          property={property}
-        />
-
-        {isVisitForm && (
-          <VisitForm
-            isFormOpen={isVisitForm}
-            closeForm={toggleVisitForm}
-            property={property}
-          />
-        )}
-
-        {isOfferForm && (
-          <OfferForm
-            isFormOpen={isOfferForm}
-            closeForm={toggleOfferForm}
-            property={property}
-          />
-        )}
-      </div>
+      <div className="right-side"></div>
     </div>
   );
 };
