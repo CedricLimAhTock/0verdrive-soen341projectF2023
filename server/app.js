ce82233f8b8c842bca1eba0ae0d918e438e791d6
SoumyBhow <soumybhow1.0@gmail.com>
2023-11-09 15:41:20 -0500

    Forgot

--- server/app.js
+++ server/app.js
@@ -1,27 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { config } from 'dotenv';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { config } from "dotenv";
 
-import signUpRoutes from './routes/auth/signup.js';
-import loginRoutes from './routes/auth/login.js';
-import userRoutes from './routes/user.js';
-import roleRoutes from './routes/role.js';
-import userRoleRoutes from './routes/user_role.js';
-import propertyRoutes from './routes/property.js';
-import propertySearchRoutes from './routes/propertySearch.js';
-import brokerSearchRoutes from './routes/brokerSearch.js';
-import visitRoutes from './routes/visit.js';
-import listingRoutes from './routes/listing.js';
-import sendEmailRoutes from './routes/send-email.js';
-import brokerRoutes from './routes/broker.js';
-import offerRoutes from './routes/offer.js';
-import propertyFavouriteRoute from './routes/property_favourite.js';
+import signUpRoutes from "./routes/auth/signup.js";
+import loginRoutes from "./routes/auth/login.js";
+import userRoutes from "./routes/user.js";
+import roleRoutes from "./routes/role.js";
+import userRoleRoutes from "./routes/user_role.js";
+import propertyRoutes from "./routes/property.js";
+import propertySearchRoutes from "./routes/propertySearch.js";
+import brokerSearchRoutes from "./routes/brokerSearch.js";
+import visitRoutes from "./routes/visit.js";
+import listingRoutes from "./routes/listing.js";
 
+import brokerRoutes from "./routes/broker.js";
+import offerRoutes from "./routes/offer.js";
+import propertyFavouriteRoute from "./routes/property_favourite.js";
 
 console.log(config());
 
-const app = express(); 
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -34,19 +33,19 @@ app.use(cors());
 //   })
 // );
 
-app.use('/signin', loginRoutes);
-app.use('/signup', signUpRoutes);
-app.use('/user', userRoutes);
-app.use('/user_role', userRoleRoutes);
-app.use('/role', roleRoutes);
-app.use('/property', propertyRoutes);
-app.use('/property/search', propertySearchRoutes);
-app.use('/broker/search', brokerSearchRoutes);
-app.use('/visit', visitRoutes);
-app.use('/listing', listingRoutes);
-app.use('/send-email', sendEmailRoutes);
-app.use('/broker', brokerRoutes);
-app.use('/offer', offerRoutes);
-app.use('/favourite', propertyFavouriteRoute);
+app.use("/signin", loginRoutes);
+app.use("/signup", signUpRoutes);
+app.use("/user", userRoutes);
+app.use("/user_role", userRoleRoutes);
+app.use("/role", roleRoutes);
+app.use("/property", propertyRoutes);
+app.use("/property/search", propertySearchRoutes);
+app.use("/broker/search", brokerSearchRoutes);
+app.use("/visit", visitRoutes);
+app.use("/listing", listingRoutes);
+
+app.use("/broker", brokerRoutes);
+app.use("/offer", offerRoutes);
+app.use("/favourite", propertyFavouriteRoute);
 
 export default app;
