5f7491890cdff29130692873fb5abe15814adff7
SoumyBhow <soumybhow1.0@gmail.com>
2023-11-09 15:57:17 -0500

    Kristi couldn't commit these himself :(

--- server/controllers/property_favourite.js
+++ server/controllers/property_favourite.js
@@ -2,154 +2,210 @@ import Property_favourite from "../models/property_favourite.js";
 import User from "../models/user.js";
 import Property from "../models/property.js";
 
-const list = async (req,res) => {
-    try {
-        let favourite = await Property_favourite.findAll({ attributes: ['id', 'property_id', 'user_id'] });
+const list = async (req, res) => {
+  try {
+    let favourite = await Property_favourite.findAll({
+      attributes: ["id", "property_id", "user_id"],
+    });
 
-        if (!favourite){
-            return res.status(404).json({});
-        }
-
-        res.status(200).send(favourite);
-
-    } catch(error){
-        console.log(error);
-        res.status(500).json({
-            message: 'Server Error'
-        });
+    if (!favourite) {
+      return res.status(404).json({});
     }
-}
 
-const listById = async (req,res) => {
-    try {
-        let favourite = await Property_favourite.findOne({
-            attributes: ['id', 'property_id', 'user_id'],
-            where: {id: req.params.id}
-        });
+    res.status(200).send(favourite);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
 
-        if (!favourite){
-            return res.status(404).json({});
-        } else {
-            res.status(200).send(favourite);
-        }
+const listById = async (req, res) => {
+  try {
+    let favourite = await Property_favourite.findOne({
+      attributes: ["id", "property_id", "user_id"],
+      where: { id: req.params.id },
+    });
 
-    } catch (error){
-        console.log(error);
-        res.status(500).json({
-            message: 'Server Error'
-        });
+    if (!favourite) {
+      return res.status(404).json({});
+    } else {
+      res.status(200).send(favourite);
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
 
+const listByUserId = async (req, res) => {
+  try {
+    let favourite = await Property_favourite.findAll({
+      attributes: ["id", "property_id", "user_id"],
+      where: { user_id: req.params.id },
+    });
 
-const listByUserId = async (req,res) => {
-    try {
-        let favourite = await Property_favourite.findAll({
-            attributes: ['id', 'property_id', 'user_id'],
-            where: {user_id: req.params.id}
-        });
-
-        if (!favourite){
-            return res.status(404).json({});
-        } else {
-            res.status(200).send(favourite);
-        }
-
-    } catch (error){
-        console.log(error);
-        res.status(500).json({
-            message: 'Server Error'
-        });
+    if (!favourite) {
+      return res.status(404).json({});
+    } else {
+      res.status(200).send(favourite);
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
 
-const listByPropertyId = async (req,res) => {
-    try{
-        let favourite = await Property_favourite.findAll({
-            attributes: ['id', 'property_id', 'user_id'],
-            where: {property_id: req.params.id}
-        });
+const listByPropertyId = async (req, res) => {
+  try {
+    let favourite = await Property_favourite.findAll({
+      attributes: ["id", "property_id", "user_id"],
+      where: { property_id: req.params.id },
+    });
 
-        if(!favourite){
-            return res.status(404).json({});
-        } else {
-            res.status(200).send(favourite);
-        }
-    } catch (error){
-        console.log(error);
-        res.status(500).json({
-            message: 'Server Error'
-        });
+    if (!favourite) {
+      return res.status(404).json({});
+    } else {
+      res.status(200).send(favourite);
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
 
+const listByUserPropertyId = async (req, res) => {
+  try {
+    let favourite = await Property_favourite.findAll({
+      attributes: ["id", "property_id", "user_id"],
+      where: { user_id: req.params.user_id, property_id: data.property_id },
+    });
+
+    if (!favourite) {
+      return res.status(404).json({});
+    } else {
+      res.status(200).send(favourite);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
 
 const create = async (req, res) => {
-    try {
-        const data = req.body;
+  try {
+    const data = req.body;
 
-        if (!data.user_id || !data.property_id) {
-            return res.status(400).json({ message: "user or property id cannot be null." });
-        }        
-
-        let temp = await User.findOne({attributes: ['id'], where: {id: data.user_id }});
-
-        if (!temp) {
-            return res.status(400).json({ message: "invalid user." });
-        }
-
-        temp = await Property.findOne({attributes: ['id'], where: {id: data.property_id }});
-
-        if (!temp) {
-            return res.status(400).json({ message: "invalid property." });
-        }
-
-        const [favourite, created] = await Property_favourite.findOrCreate({
-            attributes: ['id'],
-            where: {
-                user_id: data.user_id,
-                property_id: data.property_id
-            }
-        });
-
-        if (!created) {
-            return res.status(400).json({message: "Already exists."});
-        } else {
-            res.status(200).send(favourite);
-        }
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Server error'
-        });
+    if (!data.user_id || !data.property_id) {
+      return res
+        .status(400)
+        .json({ message: "user or property id cannot be null." });
     }
-}
 
+    let temp = await User.findOne({
+      attributes: ["id"],
+      where: { id: data.user_id },
+    });
+
+    if (!temp) {
+      return res.status(400).json({ message: "invalid user." });
+    }
+
+    temp = await Property.findOne({
+      attributes: ["id"],
+      where: { id: data.property_id },
+    });
+
+    if (!temp) {
+      return res.status(400).json({ message: "invalid property." });
+    }
+
+    const [favourite, created] = await Property_favourite.findOrCreate({
+      attributes: ["id"],
+      where: {
+        user_id: data.user_id,
+        property_id: data.property_id,
+      },
+    });
+
+    if (!created) {
+      return res.status(400).json({ message: "Already exists." });
+    } else {
+      res.status(200).send(favourite);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
 
 const destroy = async (req, res) => {
-    try {
-        const favourite = await Property_favourite.findOne({
-            attributes: ['id'],
-            where: {
-                id: req.params.id
-            }
-        });
+  try {
+    const favourite = await Property_favourite.findOne({
+      attributes: ["id"],
+      where: {
+        id: req.params.id,
+      },
+    });
 
-        if (!favourite) {
-            return res.status(404).json();
-        }
-
-        favourite.destroy();
-        
-        res.status(200).json();
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Server error'
-        });
+    if (!favourite) {
+      return res.status(404).json();
     }
-}
 
-export default {list, listById, listByUserId, listByPropertyId, create, destroy};
+    favourite.destroy();
+
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+const destroyByUserPropertyId = async (req, res) => {
+  try {
+    const favourite = await Property_favourite.findOne({
+      attributes: ["id"],
+      where: {
+        user_id: req.params.user_id,
+        property_id: req.params.property_id,
+      },
+    });
+
+    if (!favourite) {
+      return res.status(404).json();
+    }
+
+    favourite.destroy();
+
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+export default {
+  list,
+  listById,
+  listByUserId,
+  listByPropertyId,
+  listByUserPropertyId,
+  create,
+  destroy,
+  destroyByUserPropertyId,
+};
--- server/routes/property_favourite.js
+++ server/routes/property_favourite.js
@@ -1,18 +1,26 @@
 import express from "express";
-import property_favouriteController from '../controllers/property_favourite.js';
+import property_favouriteController from "../controllers/property_favourite.js";
 
 const router = express.Router();
 
-router.get('/', property_favouriteController.list);
+router.get("/", property_favouriteController.list);
 
-router.get('/:id', property_favouriteController.listById);
+router.get("/:id", property_favouriteController.listById);
 
-router.get('/user/:id', property_favouriteController.listByUserId);
+router.get("/user/:id", property_favouriteController.listByUserId);
 
-router.get('/property/:id', property_favouriteController.listByPropertyId);
+router.get("/property/:id", property_favouriteController.listByPropertyId);
+router.get(
+  "/user/:user_id/property/:property_id",
+  property_favouriteController.listByUserPropertyId
+);
 
-router.post('/', property_favouriteController.create);
+router.post("/", property_favouriteController.create);
 
-router.delete('/', property_favouriteController.destroy);
+router.delete("/", property_favouriteController.destroy);
+router.delete(
+  "/user/:user_id/property/:property_id",
+  property_favouriteController.destroyByUserPropertyId
+);
 
 export default router;
