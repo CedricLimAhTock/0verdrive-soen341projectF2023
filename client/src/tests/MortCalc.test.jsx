b03a426e1406cdcc031d484a528b20e003243c59
SoumyBhow <soumybhow1.0@gmail.com>
2023-11-16 20:18:41 -0500

    Update test

--- client/src/tests/MortCalc.test.jsx
+++ client/src/tests/MortCalc.test.jsx
@@ -4,8 +4,42 @@ import MortgageCalculator from "../components/MortgageCalculator/MortgageCalcula
 
 test("renders MortgageCalculator and updates input values", async () => {
   const onClose = () => {}; // Mock function
+
+  const property = {
+    images: [
+      {
+        original: "https://picsum.photos/id/1018/1000/600/",
+      },
+      {
+        original: "https://picsum.photos/id/1015/1000/600/",
+      },
+      {
+        original: "https://picsum.photos/id/1019/1000/600/",
+      },
+    ],
+
+    id: 1,
+    active: true,
+    civicAddress: "4",
+    aptNumber: "801",
+    street: "Heffernan",
+    neighbourhood: "Conception Bay South",
+    city: "Montreal",
+    province: "Quebec",
+    postal_code: "I6Y 1M6",
+    country: "Canada",
+    listing_type: "rent",
+    price: 9256,
+    living_area: 3122,
+    property_area: 8996,
+    num_bedrooms: 3,
+    num_bathrooms: 8,
+    num_floors: 4,
+    year_built: "1923-01-01",
+    listed_date: "2022-12-15",
+  };
   const { getByPlaceholderText, getByText } = render(
-    <MortgageCalculator isOpen={true} onClose={onClose} property={null} />
+    <MortgageCalculator isOpen={true} onClose={onClose} property={property} />
   );
 
   const homePriceInput = getByPlaceholderText("Enter home price");
@@ -14,21 +48,16 @@ test("renders MortgageCalculator and updates input values", async () => {
   );
   const interestRate = getByPlaceholderText("Enter annual interest rate");
   const loanTerm = getByPlaceholderText("Enter loan term in years");
-  const propertyTaxes = getByPlaceholderText(
-    "Enter annual property tax amount"
-  );
 
   await fireEvent.change(homePriceInput, { target: { value: "250000" } });
   await fireEvent.change(downPaymentInput, { target: { value: "20" } });
   await fireEvent.change(interestRate, { target: { value: "5" } });
   await fireEvent.change(loanTerm, { target: { value: "30" } });
-  await fireEvent.change(propertyTaxes, { target: { value: "1000" } });
 
   expect(homePriceInput.value).toBe("250000");
   expect(downPaymentInput.value).toBe("20");
   expect(interestRate.value).toBe("5");
   expect(loanTerm.value).toBe("30");
-  expect(propertyTaxes.value).toBe("1000");
 
   await new Promise((resolve) => setTimeout(resolve, 0));
 
@@ -36,5 +65,5 @@ test("renders MortgageCalculator and updates input values", async () => {
   const mortgageResult =
     getByText(/Mortgage Result:/).querySelector("span").textContent;
   // Check the mortgage result
-  expect(mortgageResult).toBe("$1073.64");
+  expect(mortgageResult).toBe("$104158");
 });
