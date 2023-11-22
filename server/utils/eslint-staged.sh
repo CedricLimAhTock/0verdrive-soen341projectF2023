1a34b6e3b1ad5a76740ecb9033aa7ae88dcca5ee
Kristi Doce <kristidoce@gmail.com>
2023-11-21 20:58:20 -0500

    udpate more eslint

--- server/utils/eslint-staged.sh
+++ server/utils/eslint-staged.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-fileList=$(git diff --diff-filter=d --cached --name-only | grep -E '\.(js|vue)$')
+fileList=$(git diff --diff-filter=d --cached --name-only | cut -d'/' -f2- | grep -E '\.(js|vue)$')
 if [ ${#fileList} -lt 1 ]; then
     echo -e "You have no staged .js or .vue files to test\n"
     exit
